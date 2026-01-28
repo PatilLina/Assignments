0d0d1c820215dabf8baaee6ab80bc70e91f36bfb
LINA PATIL <linapatil0304@gmail.com>
2026-01-28 21:06:47 +0530

    Add files via upload

---
+++ Assignment26/Program26_3.c
@@ -0,0 +1,33 @@
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name   :   program26_3.c
+//  Description :   Accept number from user and Display below pattern 
+//  Input       :   5          
+//  Output      :   1 * 2 * 3 * 4 * 5 * 
+//  Author      :   Lina vijay Patil
+//  Date        :   21/11/2025
+//
+///////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+void pattern(int iNo)
+{
+     int iCnt=0;
+
+    for(iCnt = 1;iCnt <=iNo ; iCnt++)
+    {
+        printf("%d\t*\t",iCnt);
+       
+    }
+}
+int main()
+
+{
+    int iValue=0;
+    
+    printf("Enter the number of Elements:");
+    scanf("%d",&iValue);
+    pattern(iValue);
+    return 0;
+
+}
---
+++ Assignment26/Program26_5.c
@@ -0,0 +1,35 @@
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name   :   program26_5.c
+//  Description :   Accept number from user and Display below pattern 
+//  Input       :   8         
+//  Output      :   2 4 6 8 10 11 12 14 
+//  Author      :   lina vijay patil
+//  Date        :   21/11/2025
+//
+///////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+void pattern(int iNo)
+{
+     int iCnt=0;
+
+    for(iCnt = 2; iCnt <= 2*iNo ; iCnt++)
+    {
+        if(iCnt%2==0)
+        {
+        printf(" %d\t",iCnt);
+        }
+    }
+}
+int main()
+
+{
+    int iValue=0;
+    
+    printf("Enter the number of Elements:");
+    scanf("%d",&iValue);
+    pattern(iValue);
+    return 0;
+
+}
